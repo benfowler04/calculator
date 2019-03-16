7f9366be0d244b71f1df09ee778a9e316e95bf4c
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-03-16 18:10:20 -0400

    made numbers and clear button functional

--- calc.js
+++ calc.js
@@ -1,3 +1,41 @@
+const buttons = document.querySelectorAll('button')
+const display = document.querySelector('#display')
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        let input = this.innerText
+
+        if (/\d/.test(input)) {
+            addToDisplay(input)
+        }
+        else if (input == 'C') {
+            clear()
+        }
+    })
+})
+
+function addToDisplay(number) {
+    let displayNum = display.innerText
+    if (displayNum.includes('e')) {
+        displayNum = Number(displayNum)
+    }
+    displayNum = displayNum + number
+    setDisplay(displayNum)
+}
+
+function setDisplay(displayNum) {
+    displayNum = displayNum.toString()
+    if (displayNum.length > 9) {
+        displayNum = parseInt(displayNum, 10)
+        displayNum = displayNum.toExponential(9)
+    }
+    display.innerText = displayNum
+}
+
+function clear() {
+    display.innerText = ''
+}
+
 function add (firstNum, secondNum) {
 	return firstNum + secondNum
 }
@@ -11,7 +49,13 @@ function multiply (firstNum, secondNum) {
 }
 
 function divide (firstNum, secondNum) {
-    return firstNum / secondNum
+    if (secondNum == 0) {
+        alert("Scotty can't change the laws of physics, and I can't change the laws of math!")
+        clearDisplay()
+    }
+    else {
+        return firstNum / secondNum
+    }
 }
 
 function operate(firstNum, operator, secondNum) {
